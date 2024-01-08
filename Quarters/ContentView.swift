58ae26dc79a494465b90f530376901e4fadd2001
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-01-08 14:09:00 -0600

    Initial Commit

---
+++ Quarters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quarters
+//
+//  Created by J. DeWeese on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quarters/QuartersApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuartersApp.swift
+//  Quarters
+//
+//  Created by J. DeWeese on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct QuartersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
